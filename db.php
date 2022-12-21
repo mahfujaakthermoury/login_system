4da67ed44fefedfacd736275147b11cc7ff88b47
majfujamoury <mahfujaakther186630gmail.com>
2022-12-21 11:24:14 +0600

    code

---
+++ db.php
@@ -0,0 +1,10 @@
+<?php
+    $hostname = 'localhost';
+    $username = 'root';
+    $password = '';
+    $dbname = 'login_system';
+
+    //Database Connection
+    $db = new mysqli($hostname, $username, $password, $dbname);
+
+?>
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+
+if(isset($_SESSION['log_status'])) and $_SESSION['log_status']==true{
+    //The user can access
+}else{
+    header('Location:login.php');
+} 
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>
+        Deshboard
+    </h1>
+    
+</body>
+
+</html>
